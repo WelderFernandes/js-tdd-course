ad9d9a9099f0e717a4e7291c2d58f447fafb6241
Welder N. Fernandes <welderx3@gmail.com>
2020-06-05 00:49:03 -0300

    Add editorconfig

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const a = 5;
+const b = a;
+const c = 'sdkljfhshdgfjgsfgjgsdhfjgsdjfgjsdghfjshgdjfhgvjsdhvbjfkhvsdjvfjsdgvjfgvsdjvfjksdhgvfjgsdjfgjsdhgvfjvhsdjfvjsdvfjvsdjg';
+function ab() {
+  return a, b, c;
+}
